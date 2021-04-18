cda610b91490ad1d9f4dc571a070dbd6ec53af96
amit anand <amitanandsk@gmail.com>
2021-04-19 02:13:22 +0530

    first commit from amit

---
+++ kotlinlessons/src/main/kotlin/com/lessons/kotlinlessons/KotlinlessonsApplication.kt
@@ -0,0 +1,11 @@
+package com.lessons.kotlinlessons
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinlessonsApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinlessonsApplication>(*args)
+}
---
+++ kotlinlessons/src/test/kotlin/com/lessons/kotlinlessons/KotlinlessonsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lessons.kotlinlessons
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinlessonsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
